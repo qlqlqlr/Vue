b136def95e14f2d06f18e23699e839ebf8521b35
임병은 <rlehgody1@gmail.com>
2023-11-07 17:49:46 +0900

    1107

---
+++ 04-single-file-component/vue-project/src/components/MyComponentitem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <p>MyComponentitem</p>
+    </div>
+      
+    </template>
+    
+    <script setup>
+      
+    </script>
+    
+    <style scoped>
+    
+    </style>
